bf797716561e3d85a9f41785be0d4f7d636d148a
David Quigley <daquigley4@gmail.com>
2016-04-25 16:25:05 -0400

    Configured custom templates for Scaffolding.

--- goals_with_friends/config/application.rb
+++ goals_with_friends/config/application.rb
@@ -17,6 +17,12 @@ Bundler.require(*Rails.groups)
 
 module GoalsWithFriends
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
